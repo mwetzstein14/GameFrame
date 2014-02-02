6ded6ed4e77115c06da105ec2976f39c31e38b4b
mwetzstein14 <mwetzstein14@13a452012xpf5w.myhome.westell.com>
2014-02-01 14:10:08 -1000

    Started filling in classes from UML diagram
    I finished planning out most the classes for my GameFrame framework
    except for the ones that would help to implement simple physics. I've
    started to fill in the classes from my UML diagram along with their
    instance variables and methods (and in some cases, nested classes). I
    also imported packages containing the JGame engine code that my
    framework builds on.

---
+++ src/gameframe/GameFrame.java
@@ -0,0 +1,26 @@
+package gameframe;
+
+import jgame.*;
+import jgame.impl.*;
+import jgame.platform.*;
+
+public abstract class GameFrame extends JGEngine{
+	
+	public static final double pi = 3.14159;
+	
+	private long currentFrame = 0;
+	
+	public long getCurrFrame()
+	{
+		return currentFrame;
+	}
+	
+	public void doFrame()
+	{
+		
+	}
+	
+}
+
+
+
---
+++ src/gameframe/InputManager.java
@@ -0,0 +1,36 @@
+package gameframe;
+
+public class InputManager {
+	
+	public static final int PRESS = 1;
+	public static final int HOLD = 2;
+	public static final int RELEASE = 3;
+	
+	private Button[] buttonList;
+	
+	public InputManager()
+	{
+		
+	}
+	
+	public InputManager(int[] intIDs, char[] charIDs)
+	{
+		
+	}
+	
+	public boolean checkButtonState(int id, int state)
+	{
+		
+	}
+	
+	public boolean checkButtonState(char id, int state)
+	{
+		
+	}
+	
+	class Button
+	{
+		
+	}
+
+}
